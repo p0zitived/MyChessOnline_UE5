d6267cc8fd4412482a37ebea747a613553d36455
pozitive <79838966+p0zitived@users.noreply.github.com>
2024-10-12 01:06:45 +0300

    ajunge pe azi

--- Source/MyChessOnline/Private/ChessPlayerController.cpp
+++ Source/MyChessOnline/Private/ChessPlayerController.cpp
@@ -1,2 +1,38 @@
+#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraActor.h"
 #include "ChessPlayerController.h"
 
+void AChessPlayerController::Init(bool blackTeam)
+{
+	isInBlackTeam = blackTeam;
+
+	TArray<AActor*> foundActors;
+	if (isInBlackTeam)
+	{
+		UGameplayStatics::GetAllActorsOfClassWithTag(GetWorld(), ACameraActor::StaticClass(), "BlackCamera", foundActors);
+		if (foundActors.Num() > 0)
+		{
+			cameraActor = Cast<ACameraActor>(foundActors[0]);
+		}
+	}
+	else
+	{
+		UGameplayStatics::GetAllActorsOfClassWithTag(GetWorld(), ACameraActor::StaticClass(), "WhiteCamera", foundActors);
+		if (foundActors.Num() > 0)
+		{
+			cameraActor = Cast<ACameraActor>(foundActors[0]);
+		}
+	}
+
+	if (cameraActor)
+	{
+		SetViewTarget(cameraActor);
+	}
+
+	isInited = true;
+}
+
+void AChessPlayerController::BeginPlay()
+{
+	Init(false);
+}
--- Source/MyChessOnline/Public/ChessPlayerController.h
+++ Source/MyChessOnline/Public/ChessPlayerController.h
@@ -2,11 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Camera/CameraActor.h"
 #include "ChessPlayerController.generated.h"
 
 UCLASS()
 class MYCHESSONLINE_API AChessPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+protected:
+	// FUNCTIONS
+	virtual void Init(bool blackTeam);
+	virtual void BeginPlay() override;
+	// VARIABLES
+	bool isInBlackTeam;
+	bool isInited;
+	ACameraActor* cameraActor;
 };
